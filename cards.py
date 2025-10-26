68b51528f81896bff7972afa994173036c6fc08c
asaf_ashwal <ashwalasafx@gmail.com>
2025-10-26 15:55:29 +0200

    new comper cards function

--- cards.py
+++ cards.py
@@ -14,4 +14,16 @@ def ranking(rank):
             return "J"
         case _:
              return rank
-print(card('11', "H"))
+# print(card('11', "H"))
+
+
+
+def compare_cards(p1_card: dict, p2_card: dict) -> str:
+    p1 = p1_card['value']
+    p2 = p2_card['value']
+    if p1 > p2: 
+        return 'p1'
+    elif p1 < p2: 
+        return 'p2'
+    else: return 'Value is equal'
+print(compare_cards({"rank": "J", "suite": 'S', "value":11}, {"rank": "A", "suite": "A", "value":14}))
