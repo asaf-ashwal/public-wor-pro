ecf822aa32321bde720045808b5f6a99bfe1dfb9
asaf_ashwal <ashwalasafx@gmail.com>
2025-10-26 15:09:29 +0200

    card function

---
+++ cards.py
@@ -0,0 +1,18 @@
+def card(rank,suite):
+    # rank = int(rank)
+    return {"rank": ranking(rank), "suite": suite, "value":rank}
+    
+    
+def ranking(rank):
+    match rank:
+        case '1':
+            return 'A'
+        case '13':
+            return 'K'
+        case '12':
+            return 'Q'
+        case '11':
+            return "J"
+        case _:
+             return rank
+print(card('11', "H"))
